b12ebce66c1a70289a759ba455ee8c5aa131ebd5
Monetthigpen <monet.thigpen@gmail.com>
2019-02-16 12:37:51 -0500

    listen

--- server.js
+++ server.js
@@ -12,3 +12,7 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
